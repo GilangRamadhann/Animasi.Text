59e13f21a37fe5f080fbe558c1265b6317ec837a
Gilang Ramadhan <92220215+GilangRamadhann@users.noreply.github.com>
2021-11-24 16:38:42 +0700

    Add files via upload

---
+++ animasi text.cpp
@@ -0,0 +1,73 @@
+#include<iostream> 
+#include<windows.h>
+
+using namespace std;
+int main()
+{
+	system("color 0c");
+	cout<<"\n\n\n\n\n\n\n\n\n\n\n";
+	cout<<"\t\tLOADING ";
+	char x = 222;
+	for(int i=0; i<35; i++)
+	{
+		cout<<x;
+		if(i<10)
+		Sleep(300);
+		if(i>=10 && i<20)
+    	Sleep(150);
+		if(i>=10)
+		Sleep(25);
+	}
+	system("cls");
+	system("color 0c");
+	cout<<"\n\n\n\n\n\n\n\n\n";
+	cout<<"\t\tEVEN ";
+	Sleep(500);
+	cout<<"THOUGH ";
+	Sleep(500);
+	cout<<"I'M ";
+	Sleep(500);
+	cout<<"NOT ";
+	Sleep(500);
+	cout<<"WHAT ";
+	Sleep(500);
+	cout<<"YOU ";
+	Sleep(500);
+	cout<<"EXPECTED ";
+	Sleep(500);
+	cout<<"\n";
+	cout<<"\t\tBUT ";
+	Sleep(500);
+	cout<<"I ";
+	Sleep(500);
+	cout<<"WON'T ";
+	Sleep(500);
+	cout<<"GIVE ";
+	Sleep(500);
+	cout<<"UP ";
+	Sleep(500);
+	cout<<"TO ";
+	Sleep(500);
+	cout<<"GET ";
+	Sleep(500);
+	cout<<"YOU ";
+	Sleep(500);
+	cout<<"\n ";
+	cout<<"\t\tBECAUSE ";
+	Sleep(500);
+	cout<<"I ";
+	Sleep(500);
+	cout<<"LOVE ";
+	Sleep(500);
+	cout<<"YOU ";
+	Sleep(500);
+	cout<<"\n ";
+	cout<<"\t\tAMBYAR ";
+	Sleep(500);
+	cout<<"DEK ";
+	Sleep(500);
+	cout<<":) ";
+	Sleep(500);
+	return 0;
+}
+
